35a49de5e66de139b155ea05ae2dabd854528fad
joanterm <jo_joanarg@yahoo.com>
2022-08-11 15:26:32 -0400

    Server up and running

--- api/server.js
+++ api/server.js
@@ -5,5 +5,8 @@ const server = express();
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use("/", (req, res) => {
+    res.send("Express working!")
+})
 
 module.exports = server;
