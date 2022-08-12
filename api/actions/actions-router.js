83482fdbd1a3d9520512def3d89e40714ff916cf
joanterm <jo_joanarg@yahoo.com>
2022-08-11 22:49:35 -0400

    Get actions by id works

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,14 +3,33 @@ const express = require("express")
 const router = express.Router()
 const Actions = require("./actions-model")
 
+//GET
 router.get("/", (req, res) => {
     Actions.get()
-        .then(actions => {
-            res.status(200).json(actions)
-        })
+    .then(actions => {
+        res.status(200).json(actions)
+    })
     .catch(() => {
          res.status(500).json({message: "not found"})
     })
  })
+
+ //GET BY ID
+router.get("/:id", (req, res) => {
+    Actions.get(req.params.id)
+    .then((result) => {
+        if(result == null) {
+            res.status(404).json({message: "doesn't exist"})
+            return
+        }
+        res.status(200).json(result)
+    })
+    .catch(() => {
+        res.status(500).json({message: "not found"})
+    })
+})
+
+//POST
+
 
 module.exports = router
